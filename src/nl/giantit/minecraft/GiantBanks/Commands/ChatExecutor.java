3eab82384fc7820fe4132d40e3bd44177dc9e941
Giant <giant@tpre.nl>
2012-07-26 01:45:13 +0200

    Tell chat executor about it's new command.

--- src/nl/giantit/minecraft/GiantBanks/Commands/ChatExecutor.java
+++ src/nl/giantit/minecraft/GiantBanks/Commands/ChatExecutor.java
@@ -2,6 +2,7 @@ package nl.giantit.minecraft.GiantBanks.Commands;
 
 import nl.giantit.minecraft.GiantBanks.GiantBanks;
 import nl.giantit.minecraft.GiantBanks.Commands.Chat.*;
+import nl.giantit.minecraft.GiantBanks.core.Misc.Misc;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -18,12 +19,19 @@ public class ChatExecutor {
 	public boolean exec(CommandSender sender , String[] args) {
 		Player player = (Player) sender;
 		
-		if(args.length == 0 || args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("h") || args[0].equalsIgnoreCase("?")) {
+		if(args.length == 0 || Misc.isAnyIgnoreCase(args[0], "help", "hel", "he", "h", "?", "-h")) {
 			Help.showHelp(player, args);
-		}else{
-			Store.Store(player, args);
+		}else if(Misc.isAnyIgnoreCase(args[0], "store", "stor", "sto", "st", "s", "-s")) {
+			Store.exec(player, args);
 			/*Heraut.say(player, "Ok, we have no friggin clue what you are on about, so what about we just send you our help page?");
 			help.showHelp(player, args);*/
+		}else if(Misc.isAnyIgnoreCase(args[0], "get", "ge", "g", "-g")) {
+			Get.exec(player, args);
+			/*Heraut.say(player, "Ok, we have no friggin clue what you are on about, so what about we just send you our help page?");
+			help.showHelp(player, args);*/
+		}else{
+			//Heraut.say(player, "Ok, we have no friggin clue what you are on about, so what about we just send you our help page?");
+			Help.showHelp(player, args);
 		}
 		
 		return true;
