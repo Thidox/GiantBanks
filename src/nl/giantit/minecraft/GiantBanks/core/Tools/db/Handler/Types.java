c753cf57bcc4d9aa7d3fcd22fe8b9547fda7cfeb
Giant <giant@tpre.nl>
2012-08-03 16:13:36 +0200

    Implement ability to load AccountTypes into GiantBanks.

--- src/nl/giantit/minecraft/GiantBanks/core/Tools/db/Handler/Types.java
+++ src/nl/giantit/minecraft/GiantBanks/core/Tools/db/Handler/Types.java
@@ -1,6 +1,11 @@
 package nl.giantit.minecraft.GiantBanks.core.Tools.db.Handler;
 
 import nl.giantit.minecraft.GiantBanks.Bank.AccountType;
+import nl.giantit.minecraft.GiantBanks.core.Database.Database;
+import nl.giantit.minecraft.GiantBanks.core.Database.drivers.iDriver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Types implements IHandler {
 
@@ -8,7 +13,13 @@ public class Types implements IHandler {
 		
 	@Override
 	public void init() {
-		AccountType.createAccountType(0, "default", "");
+		AccountType.createAccountType(0, "default", 20, 20);
+		iDriver db = Database.Obtain().getEngine();
+		ArrayList<HashMap<String, String>> resSet = db.select("id", "name", "maxSlots", "maxPerSlot", "items").from("#__accounttypes").execQuery();
+		
+		for(HashMap<String, String> res : resSet) {
+			AccountType.createAccountType(Integer.parseInt(res.get("id")), res.get("name"), Integer.parseInt(res.get("maxSlots")), Integer.parseInt(res.get("maxPerSlot")), res.get("items"));
+		}
 	}
 
 	@Override
