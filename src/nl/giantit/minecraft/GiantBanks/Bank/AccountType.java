70b317d167fd43184f6e579a91ff688ea2f37b5e
Giant <giant@tpre.nl>
2012-08-03 16:19:22 +0200

    Finish off AccountType for now. Implement ability to limit storable items.

--- src/nl/giantit/minecraft/GiantBanks/Bank/AccountType.java
+++ src/nl/giantit/minecraft/GiantBanks/Bank/AccountType.java
@@ -1,5 +1,10 @@
 package nl.giantit.minecraft.GiantBanks.Bank;
 
+import nl.giantit.minecraft.GiantBanks.GiantBanks;
+import nl.giantit.minecraft.GiantBanks.core.Items.ItemID;
+import nl.giantit.minecraft.GiantBanks.core.Items.Items;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AccountType {
@@ -8,16 +13,47 @@ public class AccountType {
 	private static HashMap<String, AccountType> types = new HashMap<String, AccountType>();
 	
 	private HashMap<String, Integer> data = new HashMap<String, Integer>();
+	private Items iH = GiantBanks.getPlugin().getItemHandler();
 	
 	private Integer typeID;
 	private String name;
-	private String rawData;
+	private int maxSlots = 20;
+	private int maxPerSlot = 20;
 	private Boolean isUpdated = false;
+
+	private ArrayList<String> allowed = new ArrayList<String>();
+	private ArrayList<String> disallowed = new ArrayList<String>(); 
+
+	private AccountType(Integer id, String name) {
+		this(id, name, 20, 20);
+	}
 	
-	private AccountType(Integer id, String name, String data) {
+	private AccountType(Integer id, String name, int maxSlot) {
+		this(id, name, maxSlot, 20);
+	}
+	
+	private AccountType(Integer id, String name, int maxSlot, int maxPerSlot) {
+		this(id, name, maxSlot, maxPerSlot, null);
+	}
+	
+	private AccountType(Integer id, String name, int maxSlot, int maxPerSlot, String item) {
 		this.typeID = id;
 		this.name = name;
-		this.rawData = data;
+		this.maxSlots = maxSlot;
+		this.maxPerSlot = maxPerSlot;
+		
+		if(null != item) {
+			String[] items = item.split(";");
+			for(String i : items) {
+				if(i.startsWith("+")) {
+					i = i.replaceFirst("\\+", "");
+					allowed.add(i.toLowerCase());
+				}else{
+					i = i.replace("^-", "");
+					disallowed.add(i.toLowerCase());
+				}
+			}
+		}
 	}
 	
 	public Integer getTypeID() {
@@ -28,24 +64,50 @@ public class AccountType {
 		return this.name;
 	}
 	
-	public String getRawData() {
-		//return this.getRaw();
-		return null;
+	public Boolean isStorable(ItemID item) {
+		if(item == null)
+			return false;
+		
+		return this.isStorable(iH.getItemNameByID(item.getId(), (null == item.getType() ? null : item.getType())));
+	}
+	
+	public Boolean isStorable(String item) {
+		if(item == null)
+			return false;
+		
+		if(allowed.contains(item.toLowerCase())) {
+			return true;
+		}
+		
+		if(allowed.size() > 0)
+			return false;
+		
+		return !disallowed.contains(item.toLowerCase());
 	}
 	
 	public int getMaxSlots() {
-		
-		return 20;
+		return this.maxSlots;
 	}
 	
 	public int getMaxPerSlot() {
-		
-		return 20;
+		return this.maxPerSlot;
 	}
 	
-	public static AccountType createAccountType(int id, String p, String data) {
+	public static AccountType createAccountType(int id, String p) {
+		return createAccountType(id, p, 20, 20);
+	}
+	
+	public static AccountType createAccountType(int id, String p, int maxSlot) {
+		return createAccountType(id, p, maxSlot, 20);
+	}
+	
+	public static AccountType createAccountType(int id, String p, int maxSlot, int maxPerSlot) {
+		return createAccountType(id, p, maxSlot, maxPerSlot, null);
+	}
+	
+	public static AccountType createAccountType(int id, String p, int maxSlot, int maxPerSlot, String item) {
 		if(!types.containsKey(p)) {
-			AccountType AT = new AccountType(id, p, data);
+			AccountType AT = new AccountType(id, p, maxSlot, maxPerSlot, item);
 			types.put(p, AT);
 			typeByID.put(id, p);
 			return AT;
