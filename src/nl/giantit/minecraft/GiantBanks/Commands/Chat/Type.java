a9b5dcc33c1075bd1b889f235cb97893bc2f9e73
Giant <giant@tpre.nl>
2012-08-09 04:41:45 +0200

    Explain Type command how to use Update Set sub command. Add early code for Create and Remove sub command handling.

--- src/nl/giantit/minecraft/GiantBanks/Commands/Chat/Type.java
+++ src/nl/giantit/minecraft/GiantBanks/Commands/Chat/Type.java
@@ -3,6 +3,7 @@ package nl.giantit.minecraft.GiantBanks.Commands.Chat;
 import nl.giantit.minecraft.GiantBanks.GiantBanks;
 import nl.giantit.minecraft.GiantBanks.Commands.Chat.AccountType.Storable;
 import nl.giantit.minecraft.GiantBanks.Commands.Chat.AccountType.Update.Select;
+import nl.giantit.minecraft.GiantBanks.Commands.Chat.AccountType.Update.Set;
 import nl.giantit.minecraft.GiantBanks.core.Items.Items;
 import nl.giantit.minecraft.GiantBanks.core.Misc.Messages;
 import nl.giantit.minecraft.GiantBanks.core.Misc.Misc;
@@ -16,17 +17,19 @@ public class Type {
 	
 	public static void exec(Player p, String[] args) {
 		if(args.length >= 3) {
-			if(Misc.isAnyIgnoreCase(args[1], "update", "updat", "upda", "upd", "up", "u", "-u")) {
+			if(Misc.isAnyIgnoreCase(args[1], "create", "creat", "crea", "cre", "cr", "c", "-c")) {
+				//Create.exec(p, args);
+			}else if(Misc.isAnyIgnoreCase(args[1], "update", "updat", "upda", "upd", "up", "u", "-u")) {
 				if(Misc.isAnyIgnoreCase(args[2], "select", "selec", "sele", "sel", "se", "s", "-s")) {
 					Select.exec(p, args);
 				}else if(Misc.isAnyIgnoreCase(args[2], "set", "-set")) {
-					//Set.exec(p, args);
-				}else if(Misc.isAnyIgnoreCase(args[2], "save", "sav", "sa", "-save", "-sav", "-sa")) {
-					//Save.exec(p, args);
+					Set.exec(p, args);
 				}
 			}else if(Misc.isAnyIgnoreCase(args[1], "storable", "storabl", "storab", "stora", "stor", "sto", "st", "s", "-s")) {
 				Storable.exec(p, args);
-			}
+			}else if(Misc.isAnyIgnoreCase(args[1], "remove", "remov", "remo", "rem", "re", "r", "-rem", "-rm", "-r")) {
+				//Remove.exec(p, args);
+			} 
 		}
 	}
 }
