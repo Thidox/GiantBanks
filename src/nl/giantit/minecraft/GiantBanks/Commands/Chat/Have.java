10e02660055e5826368520b2b3def3078710a5ad
Giant <giant@tpre.nl>
2012-08-14 14:52:24 +0200

    Finish the Have command.

--- src/nl/giantit/minecraft/GiantBanks/Commands/Chat/Have.java
+++ src/nl/giantit/minecraft/GiantBanks/Commands/Chat/Have.java
@@ -20,55 +20,71 @@ public class Have {
 	private static Messages mH = GiantBanks.getPlugin().getMsgHandler();
 	
 	public static void exec(Player p, String[] args) {
-		UserAccount uA = UserAccount.getUserAccount(p.getName());
-		HashMap<String, Integer> slots = uA.getItemList();
-		
-		if(slots.size() > 0) {
-			int perPage = conf.getInt("GiantBanks.global.perPage");
-			int curPag = 0;
+		if(pH.has(p, "giantbanks.bank.have")) {
+			UserAccount uA = UserAccount.getUserAccount(p.getName());
+			HashMap<String, Integer> slots = uA.getItemList();
 			
-			if(args.length >= 2) {
-				try{
-					curPag = Integer.parseInt(args[1]);
-				}catch(Exception e) {
+			if(slots.size() > 0) {
+				int perPage = conf.getInt("GiantBanks.global.perPage");
+				int curPag = 0;
+				
+				if(args.length >= 2) {
+					try{
+						curPag = Integer.parseInt(args[1]);
+					}catch(Exception e) {
+						curPag = 1;
+					}
+				}else
 					curPag = 1;
+				
+				curPag = (curPag > 0) ? curPag : 1;
+				
+				int pages = ((int)Math.ceil((double)slots.size() / (double)perPage) < 1) ? 1 : (int)Math.ceil((double)slots.size() / (double)perPage);
+				int start = (curPag * perPage) - perPage;
+				
+				if(curPag > pages) {
+					HashMap<String, String> d = new HashMap<String, String>();
+					d.put("page", String.valueOf(curPag));
+					d.put("maxPages", String.valueOf(pages));
+					
+					Heraut.say(p, mH.getMsg(msgType.ERROR, "pageExceedsMax", d));
+					return;
 				}
-			}else
-				curPag = 1;
-			
-			curPag = (curPag > 0) ? curPag : 1;
-			
-			int pages = ((int)Math.ceil((double)slots.size() / (double)perPage) < 1) ? 1 : (int)Math.ceil((double)slots.size() / (double)perPage);
-			int start = (curPag * perPage) - perPage;
-			
-			Heraut.say(p, mH.getMsg(msgType.MAIN, "accHolds"));
-			
-			HashMap<String, String> d = new HashMap<String, String>();
-			d.put("page", String.valueOf(curPag));
-			d.put("maxPages", String.valueOf(pages));
-			
-			Heraut.say(p, mH.getMsg(msgType.MAIN, "page", d));
-			
-			int i = 0;
-			for(Map.Entry<String, Integer> slot : slots.entrySet()) {
-				if(i < start) {
+				
+				Heraut.say(p, mH.getMsg(msgType.MAIN, "accHolds"));
+				
+				HashMap<String, String> d = new HashMap<String, String>();
+				d.put("page", String.valueOf(curPag));
+				d.put("maxPages", String.valueOf(pages));
+				
+				Heraut.say(p, mH.getMsg(msgType.MAIN, "page", d));
+				
+				int i = 0;
+				for(Map.Entry<String, Integer> slot : slots.entrySet()) {
+					if(i < start) {
+						i++;
+						continue;
+					}
+		
 					i++;
-					continue;
+					
+					if(i - start > perPage)
+						break;
+					
+					d = new HashMap<String, String>();
+					d.put("item", slot.getKey());
+					d.put("amt", String.valueOf(slot.getValue()));
+					
+					Heraut.say(p, mH.getMsg(msgType.MAIN, "accHoldItem", d));
 				}
-	
-				i++;
-				
-				if(i - start > perPage)
-					break;
-				
-				d = new HashMap<String, String>();
-				d.put("item", slot.getKey());
-				d.put("amt", String.valueOf(slot.getValue()));
-				
-				Heraut.say(p, mH.getMsg(msgType.MAIN, "accHoldItem", d));
+			}else{
+				Heraut.say(p, mH.getMsg(msgType.ERROR, "noStoredItems"));
 			}
 		}else{
-			Heraut.say(p, mH.getMsg(msgType.ERROR, "noStoredItems"));
+			HashMap<String, String> d = new HashMap<String, String>();
+			d.put("command", "have");
+			
+			Heraut.say(p, mH.getMsg(msgType.ERROR, "noPermissions", d));
 		}
 	}
 }
