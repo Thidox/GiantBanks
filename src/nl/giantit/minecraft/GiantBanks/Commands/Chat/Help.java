37de58454a27c7cf9acfe349858c2b0fb28f103e
Giant <giant@tpre.nl>
2012-08-14 17:01:34 +0200

    Finish help lists for now.

--- src/nl/giantit/minecraft/GiantBanks/Commands/Chat/Help.java
+++ src/nl/giantit/minecraft/GiantBanks/Commands/Chat/Help.java
@@ -19,12 +19,21 @@ public class Help {
 	
 	private static void init() {
 		entries = new ArrayList<String[]>();
-		entries.add(new String[] {"shop", "Show GiantBanks help page 1", "null"});
-		entries.add(new String[] {"shop help|h|? (page)", "Show GiantBanks help page x", "null"});
-		entries.add(new String[] {"shop sendhelp|sh [receiver] (page)", "Send GiantBanks help page x to player y", "giantbanks.admin.sendhelp"});
-		entries.add(new String[] {"shop store|s (-id [id]) (-t [type]) (-i [item]) -a [amount]", "Store an item into your bank account", "giantbanks.account.store"});
-		entries.add(new String[] {"shop get|g (-id [id]) (-t [type]) (-i [item]) -a [amount]", "Get an item from your bank account", "giantbanks.account.get"});
-		entries.add(new String[] {"shop getall|ga (-id [id]) (-t [type]) (-i [item])", "Get the entire quantity of an item from your bank account", "giantbanks.account.getall"});
+		entries.add(new String[] {"bank", "Show page 1 of items in your account", "giantbanks.bank.have"});
+		entries.add(new String[] {"bank help|? (page)", "Show GiantBanks help page x", "null"});
+		entries.add(new String[] {"bank sendhelp|sh [receiver] (page)", "Send GiantBanks help page x to player y", "giantbanks.admin.sendhelp"});
+		entries.add(new String[] {"bank store|s (-id [id]) (-t [type]) (-i [item]) -a [amount]", "Store an item into your bank account", "giantbanks.bank.store"});
+		entries.add(new String[] {"bank get|g (-id [id]) (-t [type]) (-i [item]) -a [amount]", "Get an item from your bank account", "giantbanks.bank.get"});
+		entries.add(new String[] {"bank getall|ga (-id [id]) (-t [type]) (-i [item])", "Get the entire quantity of an item from your bank account", "giantbanks.bank.getall"});
+		entries.add(new String[] {"bank have|h (page)", "Show page x of items in your account", "giantbanks.bank.have"});
+		entries.add(new String[] {"bank type|t create|c -n [name] (-ms [maxSlots]) (-mps [maxPerSlot])", "Create a new account type with the name x", "giantbanks.admin.type.create"});
+		entries.add(new String[] {"bank type|t update|u select|s [type]", "Select an account type for further modifications", "giantbanks.admin.type.select"});
+		entries.add(new String[] {"bank type|t update|u set [property] [value]", "Set property x to value y for selected account type", "giantbanks.admin.type.set"});
+		entries.add(new String[] {"bank type|t storable|s (-id [id]) (-t [type]) (-i [item]) -a [allow]", "Allow item to be stored in selected account type", "giantbanks.admin.type.storable"});
+		entries.add(new String[] {"bank account|a select|s [account]", "Select a user account for further modifications", "giantbanks.admin.account.select"});
+		entries.add(new String[] {"bank account|a type|t [type]", "Set the account type for the selected user account", "giantbanks.admin.account.settype"});
+		entries.add(new String[] {"bank account|a has", "Check items in the selected user account", "giantbanks.admin.account.has"});
+		entries.add(new String[] {"bank account|a clear|c", "Erase all items in the selected user account", "giantbanks.admin.account.clear"});
 	}
 	
 	public static void showHelp(Player player, String[] args) {
--- src/nl/giantit/minecraft/GiantBanks/Commands/Console/Help.java
+++ src/nl/giantit/minecraft/GiantBanks/Commands/Console/Help.java
@@ -15,26 +15,45 @@ import java.util.HashMap;
 public class Help {
 
 	private static ArrayList<String[]> entries = new ArrayList<String[]>();
+	private static ArrayList<String[]> cEntries = new ArrayList<String[]>();
 	private static config conf = config.Obtain();
 	private static Permission pH = GiantBanks.getPlugin().getPermHandler(); 
 	
 	private static void init() {
 		entries = new ArrayList<String[]>();
-		entries.add(new String[] {"shop", "Show GiantBanks help page 1"});
-		entries.add(new String[] {"shop help|h|? (page)", "Show GiantBanks help page x"});
-		entries.add(new String[] {"shop sendhelp|sh [receiver] (page)", "Send GiantBanks help page x to sender y"});
-		entries.add(new String[] {"shop list|l (page)", "Show all known accounts"});
+		entries.add(new String[] {"bank", "Show page 1 of items in your account", "giantbanks.bank.have"});
+		entries.add(new String[] {"bank help|? (page)", "Show GiantBanks help page x", "null"});
+		entries.add(new String[] {"bank sendhelp|sh [receiver] (page)", "Send GiantBanks help page x to player y", "giantbanks.admin.sendhelp"});
+		entries.add(new String[] {"bank store|s (-id [id]) (-t [type]) (-i [item]) -a [amount]", "Store an item into your bank account", "giantbanks.bank.store"});
+		entries.add(new String[] {"bank get|g (-id [id]) (-t [type]) (-i [item]) -a [amount]", "Get an item from your bank account", "giantbanks.bank.get"});
+		entries.add(new String[] {"bank getall|ga (-id [id]) (-t [type]) (-i [item])", "Get the entire quantity of an item from your bank account", "giantbanks.bank.getall"});
+		entries.add(new String[] {"bank have|h (page)", "Show page x of items in your account", "giantbanks.bank.have"});
+		entries.add(new String[] {"bank type|t create|c -n [name] (-ms [maxSlots]) (-mps [maxPerSlot])", "Create a new account type with the name x", "giantbanks.admin.type.create"});
+		entries.add(new String[] {"bank type|t update|u select|s [type]", "Select an account type for further modifications", "giantbanks.admin.type.select"});
+		entries.add(new String[] {"bank type|t update|u set [property] [value]", "Set property x to value y for selected account type", "giantbanks.admin.type.set"});
+		entries.add(new String[] {"bank type|t storable|s (-id [id]) (-t [type]) (-i [item]) -a [allow]", "Allow item to be stored in selected account type", "giantbanks.admin.type.storable"});
+		entries.add(new String[] {"bank account|a select|s [account]", "Select a user account for further modifications", "giantbanks.admin.account.select"});
+		entries.add(new String[] {"bank account|a type|t [type]", "Set the account type for the selected user account", "giantbanks.admin.account.settype"});
+		entries.add(new String[] {"bank account|a has", "Check items in the selected user account", "giantbanks.admin.account.has"});
+		entries.add(new String[] {"bank account|a clear|c", "Erase all items in the selected user account", "giantbanks.admin.account.clear"});
+		
+		cEntries = new ArrayList<String[]>();
+		cEntries.add(new String[] {"bank", "Show GiantNanks help page 1"});
+		cEntries.add(new String[] {"bank help|? (page)", "Show GiantBanks help page x"});
+		cEntries.add(new String[] {"bank sendhelp|sh [receiver] (page)", "Send GiantBanks help page x to player y"});
+		cEntries.add(new String[] {"bank type|t create|c -n [name] (-ms [maxSlots]) (-mps [maxPerSlot])", "Create a new account type with the name x"});
+		cEntries.add(new String[] {"bank type|t update|u select|s [type]", "Select an account type for further modifications"});
+		cEntries.add(new String[] {"bank type|t update|u set [property] [value]", "Set property x to value y for selected account type"});
+		cEntries.add(new String[] {"bank type|t storable|s (-id [id]) (-t [type]) (-i [item]) -a [allow]", "Allow item to be stored in selected account type"});
+		cEntries.add(new String[] {"bank account|a select|s [account]", "Select a user account for further modifications"});
+		cEntries.add(new String[] {"bank account|a type|t [type]", "Set the account type for the selected user account"});
+		cEntries.add(new String[] {"bank account|a has", "Check items in the selected user account"});
+		cEntries.add(new String[] {"bank account|a clear|c", "Erase all items in the selected user account"});
 	}
 	
 	public static void showHelp(CommandSender sender, String[] args) {
-		if(entries.isEmpty())
+		if(cEntries.isEmpty())
 			init();
-		
-		ArrayList<String[]> uEntries = new ArrayList<String[]>();
-		for(int i = 0; i < entries.size(); i++) {
-			String[] data = entries.get(i);
-			uEntries.add(data);
-		}
 		
 		String name = GiantBanks.getPlugin().getPubName();
 		int perPage = conf.getInt("GiantBanks.global.perPage");
@@ -51,18 +70,18 @@ public class Help {
 		
 		curPag = (curPag > 0) ? curPag : 1;
 		
-		int pages = ((int)Math.ceil((double)uEntries.size() / (double)perPage) < 1) ? 1 : (int)Math.ceil((double)uEntries.size() / (double)perPage);
+		int pages = ((int)Math.ceil((double)cEntries.size() / (double)perPage) < 1) ? 1 : (int)Math.ceil((double)cEntries.size() / (double)perPage);
 		int start = (curPag * perPage) - perPage;
 		
-		if(uEntries.size() <= 0) {
+		if(cEntries.size() <= 0) {
 			Heraut.say(sender, "[" + name + "] Sorry no help entries yet :(");
 		}else if(curPag > pages) {
 			Heraut.say(sender, "[" + name + "] My help list only has " + pages + " pages!!");
 		}else{
 			Heraut.say(sender, "[" + name + "] Help. Page: " + curPag + "/" + pages);
 
-			for(int i = start; i < (((start + perPage) > uEntries.size()) ? uEntries.size() : (start + perPage)); i++) {
-				String[] data = uEntries.get(i);
+			for(int i = start; i < (((start + perPage) > cEntries.size()) ? cEntries.size() : (start + perPage)); i++) {
+				String[] data = cEntries.get(i);
 
 				String helpEntry = data[0];
 				String description = data[1];
@@ -126,7 +145,7 @@ public class Help {
 					Heraut.say(sender, "[" + name + "] Sorry no help entries yet :(");
 					return;
 				}else if(curPag > pages) {
-					Heraut.say(sender, "[" + name + "] My help list for sender " + usr + " only has " + pages + " pages!!");
+					Heraut.say(sender, "[" + name + "] My help list for sender " + usr + " only has " + pages + " pages!");
 					return;
 				}else{
 					Heraut.say(sender, "[" + name + "] Sending help page " + curPag + " to sender " + usr);
@@ -138,8 +157,12 @@ public class Help {
 
 						String helpEntry = data[0];
 						String description = data[1];
-
-						Heraut.say(receiver, "&c/" + helpEntry + " &e-&f " + description);
+						Messages msg = GiantBanks.getPlugin().getMsgHandler();
+						HashMap<String, String> params = new HashMap<String, String>();
+						params.put("command", helpEntry);
+						params.put("description", description);
+						
+						Heraut.say(receiver, msg.getMsg(Messages.msgType.MAIN, "helpCommand", params));
 					}
 				}
 			}else{
