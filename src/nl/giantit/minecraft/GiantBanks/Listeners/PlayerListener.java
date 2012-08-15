e10b7dec7a15c3e3672c1432f9106cb3a7678c47
Giant <giant@tpre.nl>
2012-08-15 17:04:01 +0200

    Implement update checker into GiantBanks.

--- src/nl/giantit/minecraft/GiantBanks/Listeners/PlayerListener.java
+++ src/nl/giantit/minecraft/GiantBanks/Listeners/PlayerListener.java
@@ -1,5 +1,41 @@
 package nl.giantit.minecraft.GiantBanks.Listeners;
 
-public class PlayerListener {
+import nl.giantit.minecraft.GiantBanks.GiantBanks;
+import nl.giantit.minecraft.GiantBanks.core.config;
+import nl.giantit.minecraft.GiantBanks.core.Misc.Heraut;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerListener implements Listener {
+	
+	private GiantBanks plugin;
+	private config conf = config.Obtain();
+	
+	public PlayerListener(GiantBanks plugin) {
+		this.plugin = plugin;
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerJoin(final PlayerJoinEvent e) {
+		if(e.getPlayer().isOp()) {
+			if(plugin.isOutOfDate() && conf.getBoolean("GiantBanks.Updater.broadcast")) {
+				plugin.scheduleAsyncDelayedTask(new Runnable() {
+					@Override
+					public void run()
+					{
+						delayedJoin(e.getPlayer());
+					}
+				});
+			}
+		}
+	}
+	
+	public void delayedJoin(Player p) {
+		Heraut.say(p.getPlayer(), "&cA new version of GiantBanks has been released! You are currently running: " + plugin.getDescription().getVersion() + " while the latest version is: " + plugin.getNewVersion());
+	}
 
 }
--- src/nl/giantit/minecraft/GiantBanks/core/Updater/Updater.java
+++ src/nl/giantit/minecraft/GiantBanks/core/Updater/Updater.java
@@ -1,14 +1,49 @@
 package nl.giantit.minecraft.GiantBanks.core.Updater;
 
 import nl.giantit.minecraft.GiantBanks.GiantBanks;
+import nl.giantit.minecraft.GiantBanks.core.config;
+import nl.giantit.minecraft.GiantBanks.core.Misc.Heraut;
 import nl.giantit.minecraft.GiantBanks.core.Updater.Database.dbUpdate;
+
+import java.net.URL;
+import java.util.logging.Level;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Updater {
 
 	private GiantBanks plugin;
+	private int tID;
+	private config conf = config.Obtain();
+	
+	private boolean outOfDate = false;
+	private String newVersion = "";
+	
+	private void start() {
+		tID = this.plugin.scheduleAsyncRepeatingTask(new Runnable() {
+			@Override
+			public void run() {
+				newVersion = updateCheck(plugin.getDescription().getVersion());
+				if(isNewer(newVersion, plugin.getDescription().getVersion())) {
+					outOfDate = true;
+					GiantBanks.log.log(Level.WARNING, "[" + plugin.getName() + "] " + newVersion + " has been released! You are currently running: " + plugin.getDescription().getVersion());
+					if(conf.getBoolean("GiantBanks.Updater.broadcast"))
+						Heraut.broadcast("&cA new version of GiantBanks has just ben released! You are currently running: " + plugin.getDescription().getVersion() + " while the latest version is: " + newVersion, true);
+				}
+			}
+		}, 0L, 432000L);
+	}
 	
 	public Updater(GiantBanks plugin) {
 		this.plugin = plugin;
+		if(conf.getBoolean("GiantBanks.Updater.checkForUpdates")) {
+			this.start();
+		}
 	}
 	
 	public void Update(UpdateType t) {
@@ -31,6 +66,66 @@ public class Updater {
 			default:
 				break;
 		}
+	}
+	
+	public void stop() {
+		if(!Double.isNaN(tID)) {
+			plugin.getServer().getScheduler().cancelTask(tID);
+		}
+	}
+	
+	public String updateCheck(String version) {
+		String uri = "http://dev.bukkit.org/server-mods/giantbanks/files.rss";
+		try {
+			URL url = new URL(uri);
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url.openConnection().getInputStream());
+			doc.getDocumentElement().normalize();
+			Node firstNode = doc.getElementsByTagName("item").item(0);
+			if(firstNode.getNodeType() == 1) {
+				NodeList firstElementTagName = ((Element)firstNode).getElementsByTagName("title");
+				NodeList firstNodes = ((Element)firstElementTagName.item(0)).getChildNodes();
+				return firstNodes.item(0).getNodeValue().replace("GiantBanks", "").replaceAll(" \\(([a-zA-Z ]+)\\)", "").trim();
+			}
+		}catch (Exception e) {	
+		}
+		
+		return version;
+	}
+	
+	public boolean isNewer(String newVersion, String version) {
+		String[] nv = newVersion.replaceAll("\\.[a-zA-Z]+", "").split("\\.");
+		String[] v = version.replaceAll("\\.[a-zA-Z]+", "").split("\\.");
+		Boolean isNew = false;
+		Boolean prevIsEqual = false; 
+		
+		for(int i = 0; i < nv.length; i++) {
+			int tn = Integer.parseInt(nv[i]);
+			int tv = 0;
+			if(v.length - 1 >= i)
+				tv = Integer.parseInt(v[i]);
+			
+			if(tn > tv) {
+				if(i == 0 || prevIsEqual == true) {
+					isNew = true;
+					break;
+				}
+			}else if(tn == tv) {
+				prevIsEqual = true;
+			}else{
+				break;
+			}
+			
+		}
+
+		return isNew;
+	}
+	
+	public Boolean isOutOfDate() {
+		return this.outOfDate;
+	}
+	
+	public String getNewVersion() {
+		return this.newVersion;
 	}
 	
 }
